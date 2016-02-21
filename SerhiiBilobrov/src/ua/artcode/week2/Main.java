92eaf6ead8853c2647df1a65cd11515d3557998b
presly808 <presly808@gmail.com>
2016-02-21 11:54:45 +0200

    add SerhiiBilobrov

---
+++ SerhiiBilobrov/src/ua/artcode/week2/Main.java
@@ -0,0 +1,11 @@
+package ua.artcode.week2;
+
+/**
+ * Created by serhii on 21.02.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("serhii");
+    }
+}
